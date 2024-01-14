e94cdb03cd0835cdb714b7f29c4eb51e87e16f27
edwardg117 <8350193+edwardg117@users.noreply.github.com>
2024-01-14 12:38:07 +0800

    Init part 3 - Updated server builder batch file - Updated gitignore to hopefully not trach MCUpdater related things, mods and libraries.

--- Build-server.bat
+++ Build-server.bat
@@ -1,2 +1,2 @@
-Java -jar MCU-CLI-latest.jar --pack file:///dungeonmazepack.xml --path Server --server server
+Java -jar MCU-CLI-latest.jar --pack https://raw.githubusercontent.com/edwardg117/InfiniteMazeProject/main/dungeonmazepack.xml --path Server --server server
 pause
